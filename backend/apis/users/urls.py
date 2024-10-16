c0a353042b376c80453d5569578b2844703eff50
Zhiqiang Fan <166287741+fealas@users.noreply.github.com>
2024-10-16 22:48:20 +0800

    Update urls.py

--- backend/apis/users/urls.py
+++ backend/apis/users/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('look1/', views.look1, name='look1'),
+    path('lookhistory/', views.lookhistory, name='lookhistory'),
 ]
