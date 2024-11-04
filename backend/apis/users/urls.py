4e1486cfc85247387ac02c7f06050fa670ae2501
Zhiqiang Fan <166287741+fealas@users.noreply.github.com>
2024-11-04 15:50:51 +0800

    Update urls.py

--- backend/apis/users/urls.py
+++ backend/apis/users/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('look1/', views.look1, name='look1'),
     path('lookhistory/', views.lookhistory, name='lookhistory'),
+    path('commit_decision/', views.commit_decision, name='commit_decision'),
+    path('historical_decision/', views.historical_decision, name='historical_decision'),
+    path('compete_outcome/', views.compete_outcome, name='compete_outcome'),
 ]
